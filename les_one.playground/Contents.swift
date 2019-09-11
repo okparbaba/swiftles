72114ffef5df65e44999f281bb04d675435f9dbd
yannaing-lynn <rghtbossy@gmail.com>
2019-09-11 21:59:51 +0630

    sec

--- les_one.playground/Contents.swift
+++ les_one.playground/Contents.swift
@@ -1,7 +1,6 @@
 import UIKit
 
-var str = "Hello, Me"
-
+var str:String = "Hello"
 var a = 90
 print(str)
 
@@ -9,3 +8,11 @@ var b = 8
 var c = 9
 print(a+b)
 c = 90
+//str = 9
+
+let s = "Hello"
+let q = String(78)
+var u:Float = 9.0
+var d:Double = 9.09
+var e:Bool = true
+print(Int(round(8.9)))
